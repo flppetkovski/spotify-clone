7f87307556e4fab17bdc47dab83980d431dbdf79
Filip Petkovski <flp_petkovski@yahoo.com>
2021-11-07 22:24:12 +0100

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
         <Component {...pageProps} />
       </RecoilRoot>
     </SessionProvider>
